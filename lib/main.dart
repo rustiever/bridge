ff46f3ecd6ac6aa390c16259c0cefd95498a8549
rustiever <sharanrdude@gmail.com>
2020-02-12 21:10:20 +0530

    added dummy login page to test the flow of coding

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bridge/pages/loginPage.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -9,7 +10,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Bridge',
       theme: ThemeData(
-       
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Bridge Home Page'),
@@ -20,7 +20,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
- 
   final String title;
 
   @override
@@ -30,25 +29,14 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-     
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
       appBar: AppBar(
-       
         title: Text(widget.title),
       ),
       body: Center(
-       
         child: Column(
-          
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -62,9 +50,16 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => LoginPage(),
+            ),
+          );
+        },
+        tooltip: 'Lock Page',
+        child: Icon(Icons.lock),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
