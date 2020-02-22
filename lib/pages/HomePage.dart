72d14d92c3f3e67f4162cd7aa4fd6f6aa06f1285
Rustiever-Sharan <sharanrdude@gmail.com>
2020-02-22 23:10:16 +0530

    basic home page setup

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:bridge/Routes/Router.dart';
 import 'package:flutter/material.dart';
+import 'package:getflutter/getflutter.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
@@ -17,13 +18,15 @@ class _HomePageState extends State<HomePage> {
     print('${sd.size}');
     return Scaffold(
       backgroundColor: Color.fromRGBO(21, 32, 43, 1.0),
+      extendBodyBehindAppBar: true,
+
+      drawer: AppDrawer(),
+
       appBar: AppBar(
         centerTitle: true,
         title: Text(widget.title),
       ),
-
       body: Container(
-
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -57,6 +60,86 @@ class _HomePageState extends State<HomePage> {
         tooltip: 'Lock Page',
         child: Icon(Icons.lock),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class AppDrawer extends StatelessWidget {
+  Widget _createDrawerItem(
+      {IconData icon, String text, GestureTapCallback onTap}) {
+    return ListTile(
+      title: Row(
+        children: <Widget>[
+          Icon(icon),
+          Padding(
+            padding: EdgeInsets.only(left: 8.0),
+            child: Text(text),
+          )
+        ],
+      ),
+      onTap: onTap,
+    );
+  }
+
+  Widget _createHeader() {
+    return DrawerHeader(
+      margin: EdgeInsets.zero,
+      padding: EdgeInsets.zero,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          image: NetworkImage(
+            'https://images.unsplash.com/photo-1515387784663-e2e29a23f69e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80',
+          ),
+        ),
+      ),
+      child: Align(
+        alignment: Alignment.bottomCenter,
+              child: Text(
+          "Rustiever",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          _createHeader(),
+          _createDrawerItem(
+            icon: Icons.remove_from_queue,
+            text: 'Requests',
+          ),
+          _createDrawerItem(
+            icon: Icons.event,
+            text: 'Pools',
+          ),
+          _createDrawerItem(
+            icon: Icons.share,
+            text: 'Share',
+          ),
+          Divider(),
+          _createDrawerItem(icon: Icons.collections_bookmark, text: 'Steps'),
+          _createDrawerItem(icon: Icons.face, text: 'anything'),
+          _createDrawerItem(
+              icon: Icons.account_box, text: 'etc...'),
+          _createDrawerItem(icon: Icons.stars, text: 'Useful Links'),
+          Divider(),
+          _createDrawerItem(icon: Icons.bug_report, text: 'Report an issue'),
+          ListTile(
+            title: Text('0.0.1'),
+            onTap: () {},
+          ),
+        ],
+      ),
     );
   }
 }
