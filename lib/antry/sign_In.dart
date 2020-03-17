e26a3ec00ec0e2741d48a215db6daad5d9b97c92
Rustiever-Sharan <sharanrdude@gmail.com>
2020-03-17 14:17:44 +0530

    resolved issues in login/signUp

--- lib/antry/sign_In.dart
+++ lib/antry/sign_In.dart
@@ -1,4 +1,3 @@
-import 'package:bridge/Routes/Router.dart';
 import 'package:flutter/material.dart';
 
 import 'auth.dart';
--- lib/pages/SignIn/SignupPage.dart
+++ lib/pages/SignIn/SignupPage.dart
@@ -1,6 +1,7 @@
 import 'package:bridge/FirebaseServices/Auth.dart';
 import 'package:bridge/Routes/Router.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SignUp extends StatefulWidget {
   @override
@@ -8,20 +9,21 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
-  final _formKey = GlobalKey<FormState>();
-  AuthService _auth = AuthService();
-
-  TextEditingController _usn = TextEditingController();
-  TextEditingController _email = TextEditingController();
-  TextEditingController _pass1 = TextEditingController();
-  TextEditingController _pass2 = TextEditingController();
-
   static const LinearGradient SIGNUP_BACKGROUND = LinearGradient(
     begin: FractionalOffset(0.0, 0.4), end: FractionalOffset(0.9, 0.7),
     // Add one stop for each color. Stops should increase from 0 to 1
     stops: [0.1, 0.9],
     colors: [Color.fromRGBO(17, 29, 94, 1), Color.fromRGBO(178, 31, 102, 1)],
   );
+  final _formKey = GlobalKey<FormState>();
+
+  AuthService _auth = AuthService();
+  TextEditingController _userName = TextEditingController();
+  TextEditingController _usn = TextEditingController();
+  TextEditingController _email = TextEditingController();
+  TextEditingController _pass1 = TextEditingController();
+
+  TextEditingController _pass2 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -65,8 +67,48 @@ class _SignUpState extends State<SignUp> {
                             Padding(
                               padding: EdgeInsets.only(
                                   top: 10.0, bottom: 10.0, right: 00.0),
-                              child: Icon(
-                                Icons.person_outline,
+                              child: FaIcon(
+                                FontAwesomeIcons.userCircle,
+                                color: Colors.white,
+                              ),
+                            ),
+                            Expanded(
+                              child: TextFormField(
+                                controller: _userName,
+                                textAlign: TextAlign.center,
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: 'User Name',
+                                  hintStyle: TextStyle(color: Colors.white),
+                                ),
+                                // validator: ,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width,
+                        margin: const EdgeInsets.only(left: 40.0, right: 40.0),
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom: BorderSide(
+                                color: Colors.white,
+                                width: 0.5,
+                                style: BorderStyle.solid),
+                          ),
+                        ),
+                        padding: const EdgeInsets.only(left: 0.0, right: 10.0),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(
+                                  top: 10.0, bottom: 10.0, right: 00.0),
+                              child: FaIcon(
+                                FontAwesomeIcons.idBadge,
                                 color: Colors.white,
                               ),
                             ),
@@ -79,7 +121,7 @@ class _SignUpState extends State<SignUp> {
                                   hintText: 'USN',
                                   hintStyle: TextStyle(color: Colors.white),
                                 ),
-                                validator: validateUsn,
+                                validator: validateUSN,
                               ),
                             ),
                           ],
@@ -112,6 +154,7 @@ class _SignUpState extends State<SignUp> {
                             ),
                             Expanded(
                               child: TextFormField(
+                                keyboardType: TextInputType.emailAddress,
                                 controller: _email,
                                 textAlign: TextAlign.center,
                                 decoration: InputDecoration(
@@ -146,8 +189,8 @@ class _SignUpState extends State<SignUp> {
                             Padding(
                               padding: EdgeInsets.only(
                                   top: 10.0, bottom: 10.0, right: 00.0),
-                              child: Icon(
-                                Icons.lock,
+                              child: FaIcon(
+                                FontAwesomeIcons.lock,
                                 color: Colors.white,
                               ),
                             ),
@@ -188,8 +231,8 @@ class _SignUpState extends State<SignUp> {
                             Padding(
                               padding: EdgeInsets.only(
                                   top: 10.0, bottom: 10.0, right: 00.0),
-                              child: Icon(
-                                Icons.lock_open,
+                              child: FaIcon(
+                                FontAwesomeIcons.lockOpen,
                                 color: Colors.white,
                               ),
                             ),
@@ -268,14 +311,68 @@ class _SignUpState extends State<SignUp> {
     );
   }
 
+  String emailValidator(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    // if (value.isEmpty) return '*Required';
+    if (value.isEmpty) return "can't send blank line";
+    if (!regex.hasMatch(value))
+      return '*Enter a valid email';
+    else
+      return null;
+  }
+
   void firesignpakka() async {
     if (_formKey.currentState.validate()) {
       _formKey.currentState.save();
-      String res = await _auth.signUp(_usn.text, _email.text, _pass2.text);
-      if (res != null && res != 'error') {
-        Navigator.of(context).popAndPushNamed(LoginViewRoute);
+      String status = await _auth.signUp(
+          usn: _usn.text,
+          email: _email.text,
+          password: _pass2.text,
+          username: _userName.text);
+      switch (status) {
+        case '404':
+          {
+            _ackAlert(context, 'email already in use');
+            print(status + 'weak pass');
+          }
+          break;
+        case '200':
+          {
+            Navigator.of(context).popAndPushNamed(LoginViewRoute);
+          }
       }
     }
+  }
+
+  Future<void> _ackAlert(BuildContext context, String error) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(error),
+          // content: const Text('This item is no longer available'),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  String revalidatePassword(String value) {
+    if (_pass2.text.isEmpty)
+      return "can't send blank line";
+    else if (_pass2.text != _pass1.text)
+      return 'no match to what you have been written as password';
+    else
+      return null;
   }
 
   String validatePassword(String value) {
@@ -293,24 +390,14 @@ class _SignUpState extends State<SignUp> {
     }
   }
 
-  String emailValidator(String value) {
-    Pattern pattern =
-        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-    RegExp regex = new RegExp(pattern);
-    // if (value.isEmpty) return '*Required';
-    if (value.isEmpty) return "can't send blank line";
-    if (!regex.hasMatch(value))
-      return '*Enter a valid email';
-    else
+  String validateUSN(String value) {
+    Pattern pattern = r'^[0-9][a-zA-Z]{2}[0-9]{2}[a-zA-Z]{2}[0-9]{3}$';
+    RegExp regex = RegExp(pattern);
+    if (value.isEmpty) {
+      return "can't send blank line";
+    } else if (!regex.hasMatch(value)) {
+      return 'write proper usn';
+    } else
       return null;
   }
-
-  String revalidatePassword(String value) {
-    if (_pass2.text != _pass1.text)
-      return 'no match to what you have been written as password';
-    else
-      return null;
-  }
-
-  String validateUsn(String value) {}
 }
