546342a40e9eee001312860b6731661a4769d757
rustiever <sharanrdude@gmail.com>
2020-04-26 13:38:18 +0530

    removed .undodir files

--- lib/pages/Polls/AddPolls.dart
+++ lib/pages/Polls/AddPolls.dart
@@ -7,44 +7,57 @@ class AddPolls extends StatefulWidget {
 }
 
 class _AddPollsState extends State<AddPolls> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   List<Widget> _children = [];
   int _count = 0;
   List<TextEditingController> controllers = [];
   var _question = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    final _media = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                TextFormField(
-                  controller: _question,
-                  maxLines: 4,
-                  minLines: 1,
-                  decoration: new InputDecoration(
-                    labelText: "Type Your Question",
-                    fillColor: Colors.grey,
-                    border: new OutlineInputBorder(
-                      borderRadius: new BorderRadius.circular(25.0),
-                      borderSide: new BorderSide(),
+        body: Form(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Container(
+                    width: _media.width / 1.14,
+                    child: TextFormField(
+                      controller: _question,
+                      validator: validator,
+                      maxLines: 2,
+                      minLines: 1,
+                      decoration: InputDecoration(
+                        labelText: "Type Your Question",
+                        fillColor: Colors.grey,
+                        border: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(25.0),
+                          borderSide: new BorderSide(),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-                IconButton(icon: Icon(Icons.add), onPressed: _add),
-                Container(
-                  height: 200,
-                  color: Colors.deepPurpleAccent,
-                  child: ListView(children: _children),
-                ),
-              ],
-            ),
-            RaisedButton(
-              onPressed: () =>
-                  Navigator.of(context).popAndPushNamed(HomeViewRoute),
-            )
-          ],
+                  IconButton(icon: Icon(Icons.add), onPressed: _add),
+                ],
+              ),
+              Container(
+                // color: Colors.green,
+                height: _media.height / 3.23,
+                child: ListView(children: _children),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    _formKey.currentState.save();
+                    Navigator.of(context).popAndPushNamed(HomeViewRoute);
+                  }
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -56,6 +69,13 @@ class _AddPollsState extends State<AddPolls> {
     super.dispose();
   }
 
+  String validator(String value) {
+    if (value.isEmpty)
+      return "can't send blank line";
+    else
+      return null;
+  }
+
   void _add() {
     TextEditingController controller = TextEditingController();
     controllers.add(controller);
@@ -63,6 +83,7 @@ class _AddPollsState extends State<AddPolls> {
       ..add(
         TextFormField(
           controller: controller,
+          validator: validator,
           decoration: InputDecoration(
             hintText: "This is TextField $_count",
             icon: IconButton(
@@ -70,6 +91,7 @@ class _AddPollsState extends State<AddPolls> {
               onPressed: () {
                 setState(() {
                   _children = List.from(_children)..removeLast();
+                  _count--;
                 });
               },
             ),
