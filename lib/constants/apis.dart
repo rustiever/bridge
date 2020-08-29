c6d0ce7a209d4727c4a8a3588d003ee91f3222fc
Sha-Kumar <shashankneeded@gmail.com>
2020-08-29 17:02:33 +0530

    APIs file updation

--- lib/constants/apis.dart
+++ lib/constants/apis.dart
@@ -10,17 +10,30 @@ class Api {
   static const logout =
       'https://us-central1-bridge-fd58f.cloudfunctions.net/auth/logout';
 
+  //Anonymous User API Section...
+  static const anonymousPage =
+      'https://us-central1-bridge-fd58f.cloudfunctions.net/anonymous/home';
+
   //Feeds Section...
   //Do the Post...
-  static const post = 'https://us-central1-bridge-fd58f.cloudfunctions.net/home/post';
+  static const post =
+      'https://us-central1-bridge-fd58f.cloudfunctions.net/home/post';
   //Like or Dislike the post...
-  static const like = 'https://us-central1-bridge-fd58f.cloudfunctions.net/home/like';
+  static const like =
+      'https://us-central1-bridge-fd58f.cloudfunctions.net/home/like';
   //Bookmark or Un-Bookmark the Post...
-  static const bookmark = 'https://us-central1-bridge-fd58f.cloudfunctions.net/home/bookmark';
+  static const bookmark =
+      'https://us-central1-bridge-fd58f.cloudfunctions.net/home/bookmark';
   //Delete the specific Comment of a Post...
-  static const delComment = 'https://us-central1-bridge-fd58f.cloudfunctions.net/home/deleteComment';
+  static const delComment =
+      'https://us-central1-bridge-fd58f.cloudfunctions.net/home/deleteComment';
   //Edit the specific Comment of a Post...
-  static const editComment = 'https://us-central1-bridge-fd58f.cloudfunctions.net/home/editComment';
+  static const editComment =
+      'https://us-central1-bridge-fd58f.cloudfunctions.net/home/editComment';
   //Get the Comments of a specific Post...
-  static const getComments = 'https://us-central1-bridge-fd58f.cloudfunctions.net/home/getComments';
+  static const getComments =
+      'https://us-central1-bridge-fd58f.cloudfunctions.net/home/getComments';
+  //Delete the specific Post...
+  static const delPost =
+      'https://us-central1-bridge-fd58f.cloudfunctions.net/home/deletePost';
 }
